74944acad69aa49241ef96d6cf41d1a0a3802b2d
gitbash <kolhenagesh2017@gmail.com>
2021-04-29 23:21:34 +0530

    Added Employee Computation

--- empWage1.sh
+++ empWage1.sh
@@ -1,3 +1,25 @@
-#! /bin/bash
+#! /bin/bash -x
 
-echo "Welcome"
+Is_present=1
+
+empcheck=$(( RANDOM%2 ))
+
+
+if [ $empcheck -eq $Is_present ]
+then
+         echo "Emp is present"
+else
+        echo "Emp is absent"
+fi
+
+
+IS_PRESENT=1
+EMP_RATE_PER_HR=20
+empCheck=$(( RANDOM % 2 ))
+if [ $empCheck -eq $IS_PRESENT ]
+then
+     empHrs=8
+else
+     empHrs=0
+fi
+salary=$(( EMP_RATE_PER_HR * empHrs ))
